956ec36f0a13a401774e9ba43505842fa09611f1
Sanghoon Lee <nasi3611@naver.com>
2023-07-26 01:18:38 +0900

    velocity_x 패키지 사용
    velocity_x 패키지로 위젯 정리

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_shake_app/red_box.dart';
 import 'package:shake/shake.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -12,9 +12,9 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver{
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   int _counter = 0;
-
+  late ShakeDetector detector;
 
   @override
   void initState() {
@@ -23,13 +23,13 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver{
     WidgetsBinding.instance.addObserver(this);
 
     // 흔들림 감지
-    ShakeDetector.autoStart(
-      onPhoneShake: (){
-      setState(() {
-      _counter++;
-
-      });
-    }, shakeThresholdGravity: 1.5,
+    detector = ShakeDetector.autoStart(
+      onPhoneShake: () {
+        setState(() {
+          _counter++;
+        });
+      },
+      shakeThresholdGravity: 1.5,
     );
 
     super.initState();
@@ -39,7 +39,6 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver{
   @override
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
-
     super.dispose();
   }
 
@@ -52,7 +51,6 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver{
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -60,11 +58,34 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver{
       ),
       body: Center(
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              '흔들어서 카운트 앱!',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const RedBox(),
+                Column(
+                  children: [
+                    const RedBox().box.padding(EdgeInsets.all(30)).color(Colors.blue).make(),
+                    '흔들어서 카운트를 올려보세요'
+                        .text
+                        .color(Colors.red)
+                        .bold
+                        .black
+                        .size(20)
+                        .isIntrinsic
+                        .makeCentered()
+                        .box
+                        .withRounded(value: 50)
+                        .color(Colors.green)
+                        .height(150)
+                        .make()
+                        .pSymmetric(h: 20, v: 50),
+                    const RedBox(),
+                  ],
+                ),
+                const RedBox(),
+              ],
             ),
             Text(
               '$_counter',
@@ -81,4 +102,25 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver{
     );
   }
 
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        detector.startListening();
+        break;
+      case AppLifecycleState.inactive:
+        // some code
+        break;
+      case AppLifecycleState.paused:
+        detector.stopListening();
+        // some code
+        break;
+      case AppLifecycleState.detached:
+        // some code
+        break;
+      default:
+        // Handle the case when state is AppLifecycleState.resumed
+        break;
+    }
+  }
 }
